e4db1bd8fc8810af311284cd30935990ce40a212
Samuel Finlayson <samfin55@gmail.com>
2014-03-10 04:04:34 -0700

    Update server.R

--- server.R
+++ server.R
@@ -1,20 +1,42 @@
 require('survival')
+library("ggplot2")
+
+
+source("multiplot.R")
+
+connect.db <- function(username, db.name, host = "127.0.0.1") {
+  require(DBI)
+  require(RMySQL)
+  m <- dbDriver("MySQL")
+  
+  con <- dbConnect(m, username=username, dbname = db.name, host=host)
+  con
+}
+
+.libPaths("/home/samfin/R/x86_64-redhat-linux-gnu-library/3.0")
+con = connect.db(username = "", db.name="test")
+
+data <- dbGetQuery(con, "SELECT * from clinical_data")
+data$MRN <- factor(data$MRN); data$SEX <- factor(data$SEX); data$RX1 <- factor(data$RX1);
+data$SOURCE <- factor(data$SOURCE); data$MUTATION <- factor(data$MUTATION); data$R.CENSORED <- data$R.CENSORED=="T"
+
+pat.drugs <- dbGetQuery(con, "select * from drug_data group by MRN, NAME, DRUG_CLASS, INSTITUTION
+order by MRN, STR_TO_DATE(ORDERING_DATE, '%m/%d/%Y'), NAME")
+pat.drugs.full <- dbGetQuery(con, "SELECT * from drug_data order by MRN, STR_TO_DATE(ORDERING_DATE, '%m/%d/%Y'), NAME")
 
 #filepath <- "~/Documents/0 Research/Rubin/0 mrlu/"
 filepath <- "/home/samfin/ShinyApps/mrlu/"
 
-data <- read.csv(paste(filepath, "clinical_data.csv",sep=""))
-pat.drugs <- read.csv(paste(filepath, "pat_drugs.csv", sep=""))
-pat.drugs.full <- read.csv(paste(filepath, "drug_data_full.csv", sep=""))
+#data <- read.csv(paste(filepath, "clinical_data.csv",sep=""))
+#pat.drugs <- read.csv(paste(filepath, "pat_drugs.csv", sep=""))
+#pat.drugs.full <- read.csv(paste(filepath, "drug_data_full.csv", sep=""))
 
 # Function to identify the first "number.treats" drugs used on patient 'MRN'
 # Combines PACLI and Carbo when used together
 extract_drugs <- function(MRN, number.treats) {
   drug.list <- pat.drugs[which(pat.drugs$MRN == MRN), c("NAME", "ORDERING_DATE")]
-  if("PACLITAXEL" %in% drug.list && "CARBOPLATIN" %in% drug.list){
-    drug.list <- gsub("PACLITAXEL", "PACL/CARBO", x=drug.list[,"NAME"])
-    drug.list <- gsub("CARBOPLATIN", "PACL/CARBO",x=drug.list[,"NAME"])
-  }
+  
+  drug.list$NAME <- gsub("/", "/\n", x=drug.list[,"NAME"])
   
   drug.list <- drug.list[which(!duplicated(drug.list[,"ORDERING_DATE"])) ,"NAME"]
   drug.list <- paste(drug.list[1:min(number.treats,length(drug.list))], collapse=", ")
@@ -33,6 +55,10 @@ MEDICATION_TWO <- sapply(data$MRN, extract_drugs, 2)
 DRUG_CLASS <- sapply(data$MRN, extract_drug_class, 1) # REPLACE WITH SOME MAP
 DRUG_CLASS_TWO <- sapply(data$MRN, extract_drug_class, 2) # REPLACE WITH SOME MAP
 TUMOR_CHANGE <- runif(length(data$MRN),-1,1)
+
+TTNT.CENSORED <- data$DAYS_TO_NEXT_RX == 0
+data$DAYS_TO_NEXT_RX[TTNT.CENSORED] <- data$DAYS_TO_DEATH[TTNT.CENSORED]
+data <- cbind(data, TTNT.CENSORED)
 
 # Finalizes Data
 data <- cbind(data, MEDICATION, DRUG_CLASS, TUMOR_CHANGE)
@@ -130,7 +156,7 @@ shinyServer(function(input, output) {
   })
   
 #  Function to Narrow Down Dataset According to UI inputs
-  selectPats <- function(data, subgroups = TRUE, two_class = FALSE, two_drug = FALSE){
+  selectPats <- function(data, subgroups = TRUE, two_class = input$twoClass, two_drug = input$twoDrug){
     if(two_class || two_drug){
       plot.data <- data2
     }
@@ -170,14 +196,26 @@ shinyServer(function(input, output) {
   
   # Build Cox Model, Including Right-Censoring for those patients alive at end of study
   coxModel <- function(plot.data, groupBy){
-    switch(groupBy,
-           MEDICATION = coxph(Surv(DAYS_TO_DEATH, event=(!plot.data$R.CENSORED), type="right") ~ 1 + strata(MEDICATION), data=plot.data),
-           DRUG_CLASS = coxph(Surv(DAYS_TO_DEATH, event=(!plot.data$R.CENSORED), type="right") ~ 1 + strata(DRUG_CLASS), data=plot.data),
-           SEX = coxph(Surv(DAYS_TO_DEATH, event=(!plot.data$R.CENSORED), type="right") ~ 1 + strata(SEX), data=plot.data),
-           BRAF = coxph(Surv(DAYS_TO_DEATH, event=(!plot.data$R.CENSORED), type="right") ~ 1 + strata(BRAF), data=plot.data),
-           MUTATION = coxph(Surv(DAYS_TO_DEATH, event=(!plot.data$R.CENSORED), type="right") ~ 1 + strata(MUTATION), data=plot.data),
-           NRAS = coxph(Surv(DAYS_TO_DEATH, event=(!plot.data$R.CENSORED), type="right") ~ 1 + strata(NRAS), data=plot.data)
-    )
+    if(input$outcomeVar=="TTNT"){
+      switch(groupBy,
+             MEDICATION = coxph(Surv(DAYS_TO_NEXT_RX, event=(!plot.data$TTNT.CENSORED), type="right") ~ 1 + strata(MEDICATION), data=plot.data),
+             DRUG_CLASS = coxph(Surv(DAYS_TO_NEXT_RX, event=(!plot.data$TTNT.CENSORED), type="right") ~ 1 + strata(DRUG_CLASS), data=plot.data),
+             SEX = coxph(Surv(DAYS_TO_NEXT_RX, event=(!plot.data$TTNT.CENSORED), type="right") ~ 1 + strata(SEX), data=plot.data),
+             BRAF = coxph(Surv(DAYS_TO_NEXT_RX, event=(!plot.data$TTNT.CENSORED), type="right") ~ 1 + strata(BRAF), data=plot.data),
+             MUTATION = coxph(Surv(DAYS_TO_NEXT_RX, event=(!plot.data$TTNT.CENSORED), type="right") ~ 1 + strata(MUTATION), data=plot.data),
+             NRAS = coxph(Surv(DAYS_TO_NEXT_RX, event=(!plot.data$TTNT.CENSORED), type="right") ~ 1 + strata(NRAS), data=plot.data)
+      )
+    }
+    else{
+      switch(groupBy,
+             MEDICATION = coxph(Surv(DAYS_TO_DEATH, event=(!plot.data$R.CENSORED), type="right") ~ 1 + strata(MEDICATION), data=plot.data),
+             DRUG_CLASS = coxph(Surv(DAYS_TO_DEATH, event=(!plot.data$R.CENSORED), type="right") ~ 1 + strata(DRUG_CLASS), data=plot.data),
+             SEX = coxph(Surv(DAYS_TO_DEATH, event=(!plot.data$R.CENSORED), type="right") ~ 1 + strata(SEX), data=plot.data),
+             BRAF = coxph(Surv(DAYS_TO_DEATH, event=(!plot.data$R.CENSORED), type="right") ~ 1 + strata(BRAF), data=plot.data),
+             MUTATION = coxph(Surv(DAYS_TO_DEATH, event=(!plot.data$R.CENSORED), type="right") ~ 1 + strata(MUTATION), data=plot.data),
+             NRAS = coxph(Surv(DAYS_TO_DEATH, event=(!plot.data$R.CENSORED), type="right") ~ 1 + strata(NRAS), data=plot.data)
+      )
+    }
   }
 
   # Calculates desired height of plot
@@ -218,6 +256,7 @@ shinyServer(function(input, output) {
   output$boxResponse <- renderPlot({
     plot.data <- selectPats(data, two_class = input$twoClass, two_drug = input$twoDrug)
     plot.data <- plot.data[which(!is.na(plot.data$DAYS_TO_NEXT_RX)),]
+    plot.data <- subset(plot.data, TTNT.CENSORED==F)
     if(input$groupBy=='NRAS' || input$groupBy=='BRAF'){
       plot.data[,input$groupBy][which(plot.data[,input$groupBy]=='0')] <- paste(input$groupBy, " Negative", sep="")
       plot.data[,input$groupBy][which(plot.data[,input$groupBy]=='1')] <- paste(input$groupBy, " Positive",sep="")
@@ -281,6 +320,7 @@ shinyServer(function(input, output) {
     plot.data <- selectPats(data, two_class = input$twoClass, two_drug = input$twoDrug)
     plot.data <- plot.data[which(!is.na(plot.data$DAYS_TO_NEXT_RX)),]
     plot.data$MEDICATION <- factor(plot.data$MEDICATION)
+    plot.data <- subset(plot.data, TTNT.CENSORED==F)
     name.list <- sort(unique(factor(plot.data[,input$groupBy])))
     name.list <- paste(name.list,' (', table(plot.data[,input$groupBy]), ')',sep='')
     boxplot(DAYS_TO_NEXT_RX ~ MEDICATION , plot.data, cex.axis=.65,
@@ -290,20 +330,29 @@ shinyServer(function(input, output) {
             xlab="Drug Name")
     },height=900)
   
-  output$boxSummary <- renderPrint({
+  output$boxSummary <- renderTable({
     plot.data <- selectPats(data, two_class = input$twoClass, two_drug = input$twoDrug)
     plot.data <- plot.data[which(!is.na(plot.data$DAYS_TO_NEXT_RX)),]
     plot.data[,input$groupBy] <- factor(plot.data[,input$groupBy])
-    tapply(plot.data$DAYS_TO_NEXT_RX, plot.data[,input$groupBy], summary)
+    plot.data <- subset(plot.data, TTNT.CENSORED==F)
+    tab <- tapply(plot.data$DAYS_TO_NEXT_RX, plot.data[,input$groupBy], summary)
+    nms.cols <- names(tab[[1]])
+    nms.rows <- names(tab)
+    tab <- matrix(unlist(tab), ncol=length(nms.cols), byrow=T)
+    colnames(tab) <- nms.cols
+    rownames(tab) <- nms.rows
+    tab
   })
   
   output$t2ntANOVA <- renderPrint({
     plot.data <- selectPats(data, two_class = input$twoClass, two_drug = input$twoDrug)
     plot.data <- plot.data[which(!is.na(plot.data$DAYS_TO_NEXT_RX)),]
+    plot.data <- subset(plot.data, TTNT.CENSORED==F)
     if(input$groupBy=='NRAS' || input$groupBy=='BRAF'){
       plot.data[,input$groupBy][which(plot.data[,input$groupBy]=='0')] <- paste(input$groupBy, " Negative", sep="")
       plot.data[,input$groupBy][which(plot.data[,input$groupBy]=='1')] <- paste(input$groupBy, " Positive",sep="")
     }
+    plot.data <- subset(plot.data, plot.data$DAYS_TO_NEXT_RX != 0)
     if(input$groupBy == 'SEX') {
       summary(aov(DAYS_TO_NEXT_RX ~ SEX, plot.data))
     }
@@ -324,14 +373,16 @@ shinyServer(function(input, output) {
     }
   })
   
-  output$t2ntPairwiseTT <- renderPrint({
+  output$t2ntPairwiseTT <- renderTable({
     plot.data <- selectPats(data, two_class = input$twoClass, two_drug = input$twoDrug)
     plot.data <- plot.data[which(!is.na(plot.data$DAYS_TO_NEXT_RX)),]
+    plot.data <- subset(plot.data, TTNT.CENSORED==F)
     if(input$groupBy=='NRAS' || input$groupBy=='BRAF'){
       plot.data[,input$groupBy][which(plot.data[,input$groupBy]=='0')] <- paste(input$groupBy, " Negative", sep="")
       plot.data[,input$groupBy][which(plot.data[,input$groupBy]=='1')] <- paste(input$groupBy, " Positive",sep="")
     }
-    pairwise.t.test(plot.data[,"DAYS_TO_NEXT_RX"], plot.data[,input$groupBy])
+    plot.data <- subset(plot.data, plot.data$DAYS_TO_NEXT_RX != 0)
+    pairwise.t.test(plot.data[,"DAYS_TO_NEXT_RX"], plot.data[,input$groupBy])$p.value
   })
   
   output$tumorBurdenSummary <- renderPrint({
@@ -391,7 +442,7 @@ shinyServer(function(input, output) {
        
       ml.surv <- coxModel(plot.data, input$groupBy)
       mfit <- survfit(ml.surv)
-      plot(mfit, xlab="Days", ylab="Percent Living", main="Kaplain Meier",
+      plot(mfit, xlab="Days", ylab="Survival", main="Kaplain Meier",
            col=1:length(unique(plot.data[,input$groupBy])), conf.int = FALSE)
         legend(x="topright", unique(plot.data[,input$groupBy]),
                leg=name.list,
@@ -409,58 +460,94 @@ shinyServer(function(input, output) {
     coxModel(plot.data, input$groupBy)$call
   })
   
-  output$survSummary <- renderPrint({  
+  output$survSummary <- renderTable({  
     plot.data <- selectPats(data, two_class = input$twoClass, two_drug = input$twoDrug)
     if(input$groupBy=='NRAS' || input$groupBy=='BRAF'){
       plot.data[,input$groupBy][which(plot.data[,input$groupBy]=='0')] <- paste(input$groupBy, " Negative", sep="")
       plot.data[,input$groupBy][which(plot.data[,input$groupBy]=='1')] <- paste(input$groupBy, " Positive",sep="")
     }
-    survfit(coxModel(plot.data, input$groupBy))
+    summary(survfit(coxModel(plot.data, input$groupBy)))$table
   })
   
-  output$survDiffSummary <- renderPrint({
+  output$survDiffSummary <- renderTable({
     plot.data <- selectPats(data)
     if(input$groupBy=='NRAS' || input$groupBy=='BRAF'){
       plot.data[,input$groupBy][which(plot.data[,input$groupBy]=='0')] <- paste(input$groupBy, " Negative", sep="")
       plot.data[,input$groupBy][which(plot.data[,input$groupBy]=='1')] <- paste(input$groupBy, " Positive",sep="")
     }
 
-    diff_output <- switch(input$groupBy,
-                          MEDICATION = survdiff(Surv(DAYS_TO_DEATH, event=(!plot.data$R.CENSORED), type="right") ~ MEDICATION, data=plot.data),
-                          DRUG_CLASS = survdiff(Surv(DAYS_TO_DEATH, event=(!plot.data$R.CENSORED), type="right") ~ DRUG_CLASS, data=plot.data),
-                          SEX = survdiff(Surv(DAYS_TO_DEATH, event=(!plot.data$R.CENSORED), type="right") ~ SEX, data=plot.data),
-                          BRAF = survdiff(Surv(DAYS_TO_DEATH, event=(!plot.data$R.CENSORED), type="right") ~ BRAF, data=plot.data),
-                          MUTATION = survdiff(Surv(DAYS_TO_DEATH, event=(!plot.data$R.CENSORED), type="right") ~ MUTATION, data=plot.data),
-                          NRAS = survdiff(Surv(DAYS_TO_DEATH, event=(!plot.data$R.CENSORED), type="right") ~ NRAS, data=plot.data)
-    )
-     diff_output
-  })
+    if(input$outcomeVar == "TTNT"){
+      diff_output <- switch(input$groupBy,
+                            MEDICATION = survdiff(Surv(DAYS_TO_NEXT_RX, event=(!plot.data$TTNT.CENSORED), type="right") ~ MEDICATION, data=plot.data),
+                            DRUG_CLASS = survdiff(Surv(DAYS_TO_NEXT_RX, event=(!plot.data$TTNT.CENSORED), type="right") ~ DRUG_CLASS, data=plot.data),
+                            SEX = survdiff(Surv(DAYS_TO_NEXT_RX, event=(!plot.data$TTNT.CENSORED), type="right") ~ SEX, data=plot.data),
+                            BRAF = survdiff(Surv(DAYS_TO_NEXT_RX, event=(!plot.data$TTNT.CENSORED), type="right") ~ BRAF, data=plot.data),
+                            MUTATION = survdiff(Surv(DAYS_TO_NEXT_RX, event=(!plot.data$TTNT.CENSORED), type="right") ~ MUTATION, data=plot.data),
+                            NRAS = survdiff(Surv(DAYS_TO_NEXT_RX, event=(!plot.data$TTNT.CENSORED), type="right") ~ NRAS, data=plot.data)
+      )
+    }
+    else{
+      diff_output <- switch(input$groupBy,
+                            MEDICATION = survdiff(Surv(DAYS_TO_DEATH, event=(!plot.data$R.CENSORED), type="right") ~ MEDICATION, data=plot.data),
+                            DRUG_CLASS = survdiff(Surv(DAYS_TO_DEATH, event=(!plot.data$R.CENSORED), type="right") ~ DRUG_CLASS, data=plot.data),
+                            SEX = survdiff(Surv(DAYS_TO_DEATH, event=(!plot.data$R.CENSORED), type="right") ~ SEX, data=plot.data),
+                            BRAF = survdiff(Surv(DAYS_TO_DEATH, event=(!plot.data$R.CENSORED), type="right") ~ BRAF, data=plot.data),
+                            MUTATION = survdiff(Surv(DAYS_TO_DEATH, event=(!plot.data$R.CENSORED), type="right") ~ MUTATION, data=plot.data),
+                            NRAS = survdiff(Surv(DAYS_TO_DEATH, event=(!plot.data$R.CENSORED), type="right") ~ NRAS, data=plot.data)
+      )
+    }
+    
+    out <- t(data.frame(paste(deparse(diff_output$call), collapse=""),
+    diff_output$chisq,
+    (1 - pchisq(diff_output$chisq, length(diff_output$n) - 1))))
+    colnames(out) <- c("")
+    rownames(out) <- c("R Call", "Chi Squared Statistic", "P-Value")
+    out
+  }, include.colnames=FALSE)
 
   
   # Displays Age and Sex Distributions of Patients in Cohort
   output$cohortAgeSex <- renderPlot({
     plot.data <- selectPats(data)
-    layout(matrix(c(1,2),1,2,byrow=T),widths=c(1,2))
-    barplot(table(plot.data$SEX), main = "Cohort Sex Distribution")
-    hist(plot.data$AGE, main="Cohort Age Distribution", xlab="Age (Years)", ylab="# Patients",col='grey')
+    p.sex <- qplot(SEX, data = plot.data, fill = SOURCE, main = "Cohort Sex Distribution") +
+      scale_x_discrete(drop = F) + theme(axis.text=element_text(size=16),
+                                         title=element_text(size=16))
+    p.age <- qplot(AGE, data = plot.data, fill = SOURCE, main="Cohort Age Distribution") +
+      theme(axis.text=element_text(size=16),
+            title=element_text(size=16))
+    multiplot(p.sex, p.age, layout = matrix(c(1,2,2),1,3,byrow=T))
+    #print(p.sex)
   }, height = 250)
   
   # Displays Mutation Distributions of Patients in Cohort
   output$cohortGen <- renderPlot({
     plot.data <- selectPats(data)
-    par(mfrow=c(1,2))
-    barplot(table(plot.data$MUTATION), main="Cohort Positive Mutation Results Distribution")
-    barplot(table(plot.data$DRUG_CLASS), main="Cohort Treatment Drug Class Distribution",
-            names = c("CHEMO.","IMMUNO.","KINASE INH.","ANTIBODY"))
-    
+    #par(mfrow=c(1,2))
+    #barplot(table(plot.data$MUTATION), main="Cohort Positive Mutation Results Distribution")
+    #barplot(table(plot.data$DRUG_CLASS), main="Cohort Treatment Drug Class Distribution",
+    #        names = c("CHEMO-\nTHERAPY","IMMUNO-\nTHERAPY","KINASE\nINHIBITOR","THERAPEUT.\nANTIBODY"))
+    p.mut <- qplot(MUTATION, data=plot.data, fill = SOURCE,
+                   main="Cohort Positive Mutation Results Distribution") +
+      scale_x_discrete(drop = F) + theme(axis.text=element_text(size=16),
+                                         title=element_text(size=16))
+    p.drug <- qplot(DRUG_CLASS, data =plot.data, fill = SOURCE,
+                    main="Cohort Treatment Drug Class Distribution") +
+                    scale_x_discrete(labels = c("CHEMO","IMMUNO","KINASE","ANTIBOD"), drop=F) +
+      theme(axis.text=element_text(size=16),
+            title=element_text(size=16))
+    multiplot(p.mut, p.drug, layout = matrix(c(1,2),1,2,byrow=T))
   }, height = 250)
   
   # Displays Drugs Used on Patients in Cohort
   output$cohortDrugs <- renderPlot({
-    plot.data <- selectPats(data)
-   # par(mar=c(5,4,2,2),mgp = c(1, .5, 0))
-    par(mar=c(9,4,2,2))
-    barplot(table(plot.data$MEDICATION), main="Cohort Drug Distribution",las=3)                              
+    plot.data <- selectPats(data) 
+    p.med <- qplot(MEDICATION, data=plot.data, fill = SOURCE,
+          main="Cohort Drug Distribution", drop=F) +
+      theme(axis.text.y= element_text(size=16),
+            axis.text.x=element_text(angle=90,hjust=1,vjust=0.5, size=16),
+            title=element_text(size=16)
+            )
+    print(p.med)
   }, height = 400)  
   
   # Creates Table of Clinical Data for Display/Download
